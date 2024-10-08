a5042668e9ea282ff03f05e14b26d0ad87a701b5
Gjuni <kmj06178@gmail.com>
2024-10-08 10:58:00 +0900

    Bug fix

--- Practice/index.js
+++ Practice/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'
 import cors from 'cors'
 
 const app = express()
 const port = 8080
-app.use(bodyParser.json());
+// app.use(bodyParser.json()); 
 
 app.use(cors()) // cors('조건식') 모든 요청에 대해 허용
 
@@ -15,19 +15,20 @@ app.get('/', (req, res) => { // root 값 아무값도 입력하지 않은 상태
     res.send('TEST')
 });
 
-app.get('/name/:userName', (req, res) => {
-    const { id } =req.params // 요청 받은 것 중에 parmas에 받아라
+app.get('/name/:id', (req, res) => {
+
+    const { id } = req.params // 요청 받은 것 중에 parmas에 받아라
     
     // const p = req.query
     // console.log(p)
-    if (userName === 'jun') {
-        console.log('Say Hi')
-    } else if (userName === jiwan) {
-        console.log('Im the king')
+    if (id === 'jun') {
+        res.send('Say Hi')
+    } else if (id === "jiwan") {
+        res.send('Im the king')
     } else {
         console.log('make new ID')
-        user.push(userName)
-        res.send(`User ${userName} has been added`)
+        user.push(id)
+        res.send(`User ${id} has been added`)
     }
 });
 
